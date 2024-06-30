b407af2c1d593487533b45bbdcc7626392459a87
hairil-ironsoftware <115607157+hairil-ironsoftware@users.noreply.github.com>
2024-06-30 13:47:39 +0700

    update py  file

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -124,18 +124,18 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
     # ADD YOUR TEST CASES HERE ...
-	def test_read_an_account(self):
-		"""should read an account"""
-		account = self._create_accounts(1)[0]
-		resp = self.client.get(f"{BASE_URL}/{account.id}", content_type="application/json")
-		self.assertEqual(resp.status_code, status.HTTP_200_OK)
-		self.assertEqual(data["name"], account.name)
-
-	def test_get_account_list(self):
-		"""should get list of accounts"""
-		self.create_accounts(5)
-		resp = self.client.get(BASE_URL)
-		self.assertEqual(resp.status_code, status.HTTP_200_OK)
-		data = resp.get_json()
-		self.assertEqual(len(data), 5)
+    def test_read_an_account(self):
+        """should read an account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.get(f"{BASE_URL}/{account.id}", content_type="application/json")
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertEqual(data["name"], account.name)
+        
+    def test_get_account_list(self):
+        """should get list of accounts"""
+        self.create_accounts(5)
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 5)
 
