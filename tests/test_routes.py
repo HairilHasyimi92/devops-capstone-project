577d2f3652942cca038be28b71a40be594222ceb
hairil-ironsoftware <115607157+hairil-ironsoftware@users.noreply.github.com>
2024-07-20 11:29:49 +0700

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -126,7 +126,6 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-
     # ADD YOUR TEST CASES HERE ...
     def test_get_account_list(self):
         """should get list of accounts"""
